2f75cd427fdfd353a93d7eb1b9c321f3e01c6948
Nitish Kalra <nitishkalraa@gmail.com>
2021-01-30 15:07:46 +0530

    Add files via upload

---
+++ js/ground.js
@@ -0,0 +1,21 @@
+class Ground {
+  constructor(x, y, w, h) {
+    var options = {
+      isStatic: true,
+    };
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.image=loadImage("images/ground.jpg");
+    World.add(world, this.body);
+  }
+  display() {
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    imageMode(CENTER);
+    image(this.image,0, 0, this.w, this.h);
+    pop();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,75 @@
+// MADE BY NITISH
+// WILL COMPLETE ALL PROJECTS AND OTHER STUFF TILL SUNDAY 
+const Engine = Matter.Engine,
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body,
+  Constraint = Matter.Constraint;
+
+var treeObj, stone, groundObject, launcher;
+var mangoes = [];
+var bg;
+
+function preload() {
+  boy = loadImage("images/boy.png");
+  treeObj=loadImage("images/tree.png");
+  bg=loadImage("images/bg.jpg");
+}
+
+function setup() {
+  createCanvas(1300, 600);
+  var engine = Engine.create();
+  world = engine.world;
+
+  stone = new Stone(0, 0, 40);
+  for (var i = 0; i < 8; i++) {
+    if (i < 5) mangoes[i] = new Mango(random(900, 1200), 230, 30);
+    else mangoes[i] = new Mango(random(1000, 1100), 130, 30);
+  }
+
+  groundObject = new Ground(width / 2, height - 30, width, 70);
+  launcher = new Launcher(stone.body, { x: 275, y: 420 });
+
+  Engine.run(engine);
+}
+
+function draw() {
+  background(bg);
+  image(boy, 200,295, 150, 250);
+  image(treeObj, 800, 0,500,600);
+
+  mangoes.forEach((mango) => {
+    mango.display();
+    collision(stone, mango);
+  });
+
+  stone.display();
+  groundObject.display();
+  launcher.display();
+}
+
+function mouseDragged() {
+  if(stone.body.position.x<300)
+  Body.setPosition(stone.body, { x: mouseX, y: mouseY });
+}
+
+function mouseReleased() {
+  launcher.fly();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    Body.setPosition(stone.body, { x: 255, y: 420 });
+    launcher.attach(stone.body);
+  }
+}
+
+function collision(stone, mango) {
+  var distance = dist(
+    stone.body.position.x,
+    stone.body.position.y,
+    mango.body.position.x,
+    mango.body.position.y
+  );
+  if (distance <= mango.r + stone.r) Matter.Body.setStatic(mango.body, false);
+}
